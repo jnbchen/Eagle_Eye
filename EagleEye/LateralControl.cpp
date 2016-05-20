40bdb5614f1e0779c87af4e36572da44df123081
Johannes <uadxb@student.kit.edu>
2016-05-20 21:27:20 +0200

    Implement calculation of distance, difference angle, curvature in lateral control

--- EagleEye/LateralControl.cpp
+++ EagleEye/LateralControl.cpp
@@ -1,48 +1,89 @@
 #include "../Elementary/KogmoThread.h"
 #include "../Elementary/PluginFactory.h"
 #include "../Blackboard/Blackboard.h"
+#include "../Elementary/Angle.h"
+#include "BezierCurve.h"
+#include "math.h"
 
 namespace DerWeg {
 
+    struct ControllerInput {
+        double distance;        ///< Distance to reference curve
+        Angle diff_angle;        ///< Angle difference between ego and curve
+        double curvature;       ///< Curvature of reference curve
+
+        ControllerInput(double dist, Angle angle, double curv) : distance(dist), diff_angle(angle), curvature(curv) {;} /// Constructor
+    };
+
+
+
   /** LateralControl */
   class LateralControl : public KogmoThread {
-  public:
-    LateralControl () {;}
-    ~LateralControl () {;}
-    void execute () {
-      try{
-        while (true) {
 
-          // to be implemented ...
+    private:
+        double lastProjectionParameter = 0;
+        BezierCurve bc;
 
-          /*
+    public:
+        LateralControl () {;}
+        ~LateralControl () {;}
 
-          // Struct to contain the input parameter for lateral control
-            struct ControlInput {
-                double distance;        ///< Distance to reference curve
-                Angle diff_angle;        ///< Angle difference between ego and curve
-                double curvature;       ///< Curvature of reference curve
+        //ControllerInput calculate_curve_data(const BezierCurve& bc, const State& state);
+
+        void execute () {
+          try{
+            while (true) {
+
+              if (bc != BBOARD->getReferenceTrajectory().path) {
+                bc = BBOARD->getReferenceTrajectory().path;
+                lastProjectionParameter = 0;
+              }
+
+              ControllerInput input = calculate_curve_data(BBOARD->getState());
+
+              /*
+              Stanley-Controller here
+              */
+
+              //boost::this_thread::sleep(boost::posix_time::milliseconds(20));
+              boost::this_thread::interruption_point();
             }
-
-
-          ControlInput control_input(const BezierCurve& bc, const State& state) const throw() {
-            // what eevaaaaaa ...
-            }
-
-          if (bc == BBOARD->getReferenceCurve().path) {
-            new_start = t;
-          }
-          else {
-            bc = BBOARD->getReferenceCurve().path;
-            new_start = 0;
-          }
-          */
-
-          //boost::this_thread::sleep(boost::posix_time::milliseconds(20));
-          boost::this_thread::interruption_point();
+          }catch(boost::thread_interrupted&){;}
         }
-      }catch(boost::thread_interrupted&){;}
-    }
+
+        /* Calculates the distance from the current position to the bezier curve, the angle of the vehicle with respect to the curve,
+        and the curvature of the curve. Those values are needed for the controller. */
+        ControllerInput calculate_curve_data(const State& state)  {
+            lastProjectionParameter = bc.project(state.position, lastProjectionParameter);
+
+            //evaluate bezier curve and derivatives at the projection parameter
+            Vec f = bc(lastProjectionParameter);
+            Vec df = bc.prime(lastProjectionParameter);
+            Vec ddf = bc.double_prime(lastProjectionParameter);
+
+            //Calculate distance form current position to curve
+            Vec diff = f - state.position;
+            double distance = diff.length();
+
+            //Calculate difference angle between vehicle and curve
+            double phi;
+            if (df.x != 0 && abs(df.y/df.x) < 1) {
+                //atan2 takes the direction into account
+                phi = atan2(df.y, df.x);
+            } else {
+                //avoid singularities by rotating coordinate system for 2nd and 4th quadrant
+                phi = M_PI/2 + atan2(-df.x, df.y);
+            }
+            Angle diff_angle = Angle::rad_angle(phi) - state.orientation;
+
+            //Calculate the curvature of the bezier curve
+            double curvature_numerator = ddf.y * df.x - ddf.x * df.y;
+            double curvature_denominator = pow(df.squared_length(), 3.0/2);
+            double curvature = curvature_numerator / curvature_denominator;
+
+            return ControllerInput(distance, diff_angle, curvature);
+        }
+
   };
 
 } // namespace DerWeg

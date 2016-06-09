f54d5a9462017f19dec64f8a75de29888a077ef8
kal5 <kal5@mrt213.mrt.uni-karlsruhe.de>
2016-06-09 20:57:26 +0200

    Merge trafficlight detection and localization (fh)

--- EagleEye/TrafficLightDetection.cpp
+++ EagleEye/TrafficLightDetection.cpp
@@ -5,9 +5,69 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+#include "../Elementary/Angle.h"
+#include "../Elementary/Vec.h"
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+
 using namespace cv;
 
 namespace DerWeg {
+
+    /** Simple implemenation of a 2x2 Matrix */
+    class CovMat {
+        private:
+            double a, b, c, d;
+            /* C = (a, b;
+                    c, d)
+            */
+
+        public:
+            CovMat() {}
+            CovMat(const double a0, const double b0,
+                   const double c0, const double d0) :
+                a(a0), b(b0), c(c0), d(d0) {}
+            CovMat(std::vector<double> v) :
+                a(v[0]), b(v[1]), c(v[2]), d(v[3]) {}
+            ~CovMat() {}
+
+            CovMat operator+ (const CovMat& C) const {
+                // Matrix-Matrix-Sum
+                return CovMat(a+C.a, b+C.b, c+C.c, d+C.d);
+            }
+
+            CovMat operator* (const CovMat& C) {
+                // Matrix-Matrix-Product
+                return CovMat(a*C.a + b*C.c, a*C.b + b*C.d,
+                               c*C.a + d*C.c, c*C.b + d*C.d);
+            }
+
+            Vec operator* (const Vec& v) const {
+                // Matrix-Vector-Product
+                return Vec(a*v.x + b*v.y, c*v.x + d*v.y);
+            }
+
+            double det() const {
+                // Determinant
+                return a*d - b*c;
+            }
+
+            CovMat inv() const {
+                // Inverse
+                double det = this->det();
+                return CovMat(d/det, -b/det, -c/det, a/det);
+                }
+
+            CovMat t() const {
+                // Transpose
+                return CovMat(a, c, b, d);
+            }
+
+        };
+
+
 
   /** TrafficLightDetection */
   class TrafficLightDetection : public KogmoThread {
@@ -21,6 +81,12 @@ namespace DerWeg {
     int upper_red_h1; int upper_red_h2; int upper_red_v1; int upper_red_v2;
     int yellow_h1; int yellow_h2; int yellow_v1; int yellow_v2;
     int green_h1; int green_h2; int green_v1; int green_v2;
+
+    // Localization variables
+    State state;
+    std::vector<Vec> tl_mean;
+    std::vector<CovMat> tl_covar;
+    CovMat C_measure_prototype;
 
   public:
     /** Konstruktor initialisiert den Tiefenschaetzer */
@@ -50,13 +116,32 @@ namespace DerWeg {
         cfg.get("Ampelerkennung::yellow_v1", yellow_v1);
         cfg.get("Ampelerkennung::yellow_v2", yellow_v2);
         cfg.get("Ampelerkennung::stopdistance", stopdis);
+
+        // Localization init
+        std::vector<double> init_mean;
+        cfg.get("TrafficLightDetection::init_mean", init_mean);
+        for (unsigned int i = 0; i < init_mean.size()/2; ++i) {
+            tl_mean[i] = Vec(init_mean[i*2], init_mean[i*2 + 1]);
+        }
+
+        std::vector<double> init_cov;
+        cfg.get("TrafficLightDetection::init_cov", init_cov);
+        for (unsigned int i = 0; i < init_mean.size()/2; ++i) {
+            tl_covar[i] = CovMat(init_cov);
+        }
+
+        std::vector<double> covmeasure;
+        cfg.get("TrafficLightDetection::C_measure", covmeasure);
+        C_measure_prototype = CovMat(covmeasure);
     }
+
 
     void execute () {
       try{
         while (true) {
           BBOARD->waitForImage();
           ib = BBOARD->getImage();
+          state = BBOARD->getState();
 
           // starte die Tiefenberechnung
           stereoGPU.runStereoMatching (ib.image, ib.image_right);
@@ -66,8 +151,10 @@ namespace DerWeg {
           stereoGPU.getStereoResults (depth,conf);
 
           //==================================================================
+          // DETECTION
+
           // windows
-          Mat image = ib.image.copy();
+          Mat image(ib.image);
           CvRect rec;
           rec.x=0,rec.y=140,rec.width=659,rec.height=200;
           image = image(rec);
@@ -97,9 +184,9 @@ namespace DerWeg {
           erode(green_hue_range, green_hue_range, element);
 
           //edge detection
-          Mat red_hue_range = red_hue_range >= sliderPos;
-          Mat yellow_hue_range = yellow_hue_range >= sliderPos;
-          Mat green_hue_range = green_hue_range >= sliderPos;
+          red_hue_range = red_hue_range >= sliderPos;
+          yellow_hue_range = yellow_hue_range >= sliderPos;
+          green_hue_range = green_hue_range >= sliderPos;
 
           vector<vector<Point> > contours_red;
           vector<vector<Point> > contours_yellow;
@@ -164,10 +251,10 @@ namespace DerWeg {
 
           //distance calculate
           float distance = 0.0;
-          for(i = 0; i <= center.size(); i++){
+          for(unsigned int i = 0; i <= center.size(); i++){
               distance += depth.at<float>(center[i].x, center[i].y);
           }
-          float distance = distance/center.size();
+          distance = distance/center.size();
 
           //Velocity control
           Velocity dv;
@@ -177,7 +264,52 @@ namespace DerWeg {
               dv.velocity = 1;
 
           BBOARD->setDesiredVelocity(dv);
+
+
           //==================================================================
+          // LOCALIZATION
+
+          // Assumed postion of detected traffic lights in car coordinates
+          // COMES FROM ABOVE
+          Vec tl_position(1, 0);
+
+          // Logic for reasoning which traffic light
+          // Depends on which point car comes from {1, 2, 3, 4}
+          // TO BE IMPLEMENTED
+          int tl_index = 1;
+
+          // Scale measurement covariance matrix in car coordinates
+          CovMat C_measure = C_measure_prototype;
+
+          // Convert to global coordinates
+          tl_position += state.position;
+          double alpha = state.orientation.get_rad_pi() +
+                          std::atan2(tl_position.y, tl_position.x);
+          double alpha_sin = std::sin(alpha);
+          double alpha_cos = std::cos(alpha);
+          CovMat rotation(alpha_cos, alpha_sin, -alpha_sin, alpha_cos);
+          C_measure = rotation.t() * (C_measure * rotation);
+
+          // Update estimate
+          CovMat C_inv = (C_measure + tl_covar[tl_index]).inv();
+          tl_mean[tl_index] = C_measure * (C_inv * tl_mean[tl_index]) +
+                              tl_covar[tl_index] * (C_inv * tl_position);
+          tl_covar[tl_index] = C_measure * (C_inv * tl_covar[tl_index]);
+
+          // Plot measured and estimated positions in AnicarViewer
+          // measured: red
+          // estimated: dark green
+          std::stringstream tl_pos;
+          tl_pos << "thick red dot "
+                 << tl_position.x << " " << tl_position.y << std::endl;
+          for (unsigned int i = 0; i < tl_mean.size(); ++i) {
+              tl_pos << "thick darkGreen dot "
+                     << tl_mean[i].x << " " << tl_mean[i].y << std::endl;
+          }
+          BBOARD->addPlotCommand(tl_pos.str());
+
+          //==================================================================
+
           boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
           boost::this_thread::interruption_point();
         }

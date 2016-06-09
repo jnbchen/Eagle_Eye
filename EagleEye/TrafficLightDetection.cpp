771a7e35166d67b4a2385b29127d74ce004e7f62
kal5 <kal5@mrt213.mrt.uni-karlsruhe.de>
2016-06-09 20:20:07 +0200

    Add first traffic light localization (fh)
    In TrafficLightDetection there is now a localization, which estimates
    the traffic light positions by iterative averaging over the
    measruements. The covariances are incoporated in the calculations.
    
    The initial estimated positions, along with the initial covariance can
    be specified in the config. Also there: the covariance of the
    measurements.
    
    The currently measure position together with the estimated ones is
    displayed in the AnicarViewer as dots.
    (measurement = red, estmates = dark green)

--- EagleEye/TrafficLightDetection.cpp
+++ EagleEye/TrafficLightDetection.cpp
@@ -6,8 +6,68 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+#include "../Elementary/Angle.h"
+#include "../Elementary/Vec.h"
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+
 
 namespace DerWeg {
+
+    /** Simple implemenation of a 2x2 Matrix */
+    class Mat {
+        private:
+            double a, b, c, d;
+            /* C = (a, b;
+                    c, d)
+            */
+
+        public:
+            Mat() {}
+            Mat(const double a0, const double b0,
+                   const double c0, const double d0) :
+                a(a0), b(b0), c(c0), d(d0) {}
+            Mat(std::vector<double> v) :
+                a(v[0]), b(v[1]), c(v[2]), d(v[3]) {}
+            ~Mat() {}
+
+            Mat operator+ (const Mat& C) const {
+                // Matrix-Matrix-Sum
+                return Mat(a+C.a, b+C.b, c+C.c, d+C.d);
+            }
+
+            Mat operator* (const Mat& C) {
+                // Matrix-Matrix-Product
+                return Mat(a*C.a + b*C.c, a*C.b + b*C.d,
+                               c*C.a + d*C.c, c*C.b + d*C.d);
+            }
+
+            Vec operator* (const Vec& v) const {
+                // Matrix-Vector-Product
+                return Vec(a*v.x + b*v.y, c*v.x + d*v.y);
+            }
+
+            double det() const {
+                // Determinant
+                return a*d - b*c;
+            }
+
+            Mat inv() const {
+                // Inverse
+                double det = this->det();
+                return Mat(d/det, -b/det, -c/det, a/det);
+                }
+
+            Mat t() const {
+                // Transpose
+                return Mat(a, c, b, d);
+            }
+
+        };
+
+
 
   /** TrafficLightDetection */
   class TrafficLightDetection : public KogmoThread {
@@ -15,6 +75,12 @@ namespace DerWeg {
     ImageBuffer ib;
     DerWeg::StereoGPU stereoGPU;
     cv::Mat depth, conf, rect;
+
+    // Localization variables
+    State state;
+    std::vector<Vec> tl_mean;
+    std::vector<Mat> tl_covar;
+    Mat C_measure_prototype;
 
   public:
     /** Konstruktor initialisiert den Tiefenschaetzer */
@@ -25,11 +91,34 @@ namespace DerWeg {
     ~TrafficLightDetection () {}
 
 
+    void init (const ConfigReader& cfg) {
+
+
+        // Localization init
+        std::vector<double> init_mean;
+        cfg.get("TrafficLightDetection::init_mean", init_mean);
+        for (unsigned int i = 0; i < init_mean.size()/2; ++i) {
+            tl_mean[i] = Vec(init_mean[i*2], init_mean[i*2 + 1]);
+        }
+
+        std::vector<double> init_cov;
+        cfg.get("TrafficLightDetection::init_cov", init_cov);
+        for (unsigned int i = 0; i < init_mean.size()/2; ++i) {
+            tl_covar[i] = Mat(init_cov);
+        }
+
+        std::vector<double> covmeasure;
+        cfg.get("TrafficLightDetection::C_measure", covmeasure);
+        C_measure_prototype = Mat(covmeasure);
+    }
+
+
     void execute () {
       try{
         while (true) {
           BBOARD->waitForImage();
           ib = BBOARD->getImage();
+          state = BBOARD->getState();
 
           // starte die Tiefenberechnung
           stereoGPU.runStereoMatching (ib.image, ib.image_right);
@@ -45,6 +134,48 @@ namespace DerWeg {
 
 
 
+
+          //==================================================================
+          // LOCALIZATION
+
+          // Assumed postion of detected traffic lights in car coordinates
+          // COMES FROM ABOVE
+          Vec tl_position(1, 0);
+
+          // Logic for reasoning which traffic light
+          // Depends on which point car comes from {1, 2, 3, 4}
+          // TO BE IMPLEMENTED
+          int tl_index = 1;
+
+          // Scale measurement covariance matrix in car coordinates
+          Mat C_measure = C_measure_prototype;
+
+          // Convert to global coordinates
+          tl_position += state.position;
+          double alpha = state.orientation.get_rad_pi() +
+                          std::atan2(tl_position.y, tl_position.x);
+          double alpha_sin = std::sin(alpha);
+          double alpha_cos = std::cos(alpha);
+          Mat rotation(alpha_cos, alpha_sin, -alpha_sin, alpha_cos);
+          C_measure = rotation.t() * (C_measure * rotation);
+
+          // Update estimate
+          Mat C_inv = (C_measure + tl_covar[tl_index]).inv();
+          tl_mean[tl_index] = C_measure * (C_inv * tl_mean[tl_index]) +
+                              tl_covar[tl_index] * (C_inv * tl_position);
+          tl_covar[tl_index] = C_measure * (C_inv * tl_covar[tl_index]);
+
+          // Plot measured and estimated positions in AnicarViewer
+          // measured: red
+          // estimated: dark green
+          std::stringstream tl_pos;
+          tl_pos << "thick red dot "
+                 << tl_position.x << " " << tl_position.y << std::endl;
+          for (unsigned int i = 0; i < tl_mean.size(); ++i) {
+              tl_pos << "thick darkGreen dot "
+                     << tl_mean[i].x << " " << tl_mean[i].y << std::endl;
+          }
+          BBOARD->addPlotCommand(tl_pos.str());
 
 
           //==================================================================

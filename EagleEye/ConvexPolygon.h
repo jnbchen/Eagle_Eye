e6eb83e8f7706ed478bc9a9aaedf2d39e7ab0abd
kal5 <kal5@mrt219.mrt.uni-karlsruhe.de>
2016-07-14 17:41:16 +0200

    Implement indicator control and regions where indicators should flash (jf)

---
+++ EagleEye/ConvexPolygon.h
@@ -0,0 +1,40 @@
+
+#ifndef _DerWeg_CONVEXPOLYGON_H__
+#define _DerWeg_CONVEXPOLYGON_H__
+
+#include "../Elementary/ThreadSafeLogging.h"
+#include "../Elementary/Vec.h"
+
+namespace DerWeg {
+
+class ConvexPolygon {
+private:
+    vector<Vec> points;
+
+public:
+    ConvexPolygon(vector<double> coords) {
+        if (coords.size() % 2 != 0) {
+            EOUT("Error: uneven number of coordinates in ConvexPolygon\n");
+        } else {
+            for (int i=0; i < coords.size(); i += 2) {
+                points.push_back(Vec(coords[i], coords[i+1]));
+            }
+        }
+    }
+
+    bool isInside(Vec pos) {
+        bool is_inside = true;
+        // check for each edge, if pos is left of edge
+        for (int i=0; i<points.size(); i++) {
+            Vec& p_begin = points[i];
+            Vec& p_end = points[(i+1)%points.size()];
+            bool left = (pos - p_begin) * (p_end - p_begin).rotate_quarter() > 0;
+            is_inside = is_inside && left;
+        }
+        return is_inside;
+    }
+};
+
+} // Namespace
+
+#endif // _DerWeg_CONVEXPOLYGON_H__
---
+++ EagleEye/IndicatorControl.cpp
@@ -0,0 +1,44 @@
+#include "../Elementary/KogmoThread.h"
+#include "../Elementary/PluginFactory.h"
+#include "../Blackboard/Blackboard.h"
+
+using namespace std;
+
+namespace DerWeg {
+
+  /** IndicatorControl */
+  class IndicatorControl : public KogmoThread {
+
+    private:
+        ConvexPolygon intersection_region;
+        ConvexPolygon turnout_region;
+
+    public:
+        IndicatorControl () {;}
+        ~IndicatorControl () {;}
+
+
+	void init(const ConfigReader& cfg) {
+        vector<double> tmp1;
+	}
+
+
+    void execute () {
+      try{
+        LOUT("Modul Indicator\n");
+        while (true) {
+
+            boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+            boost::this_thread::interruption_point();
+        }
+      }catch(boost::thread_interrupted&){;}
+    }
+
+} // namespace DerWeg
+
+namespace {
+
+  // Plugin bei der Factory anmelden
+  static DerWeg::PluginBuilder<DerWeg::KogmoThread, DerWeg::IndicatorControl> application ("IndicatorControl");
+
+}
